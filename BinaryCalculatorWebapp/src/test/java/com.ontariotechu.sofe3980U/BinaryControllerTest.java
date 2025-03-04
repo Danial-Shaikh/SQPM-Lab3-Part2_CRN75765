fa84f8b4a024ff1ddc66dd4b9fd9b04dd39df7ea
Danial Shaikh <80362779+Danial-Shaikh@users.noreply.github.com>
2025-03-03 22:15:15 -0500

    BinaryControllerTest.java

--- BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
+++ BinaryCalculatorWebapp/src/test/java/com.ontariotechu.sofe3980U/BinaryControllerTest.java
@@ -1,40 +1,39 @@
 package com.ontariotechu.sofe3980U;
-import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 
-import org.junit.runner.RunWith;
-
-import org.junit.*;
-import org.junit.runner.*;
-import org.springframework.beans.factory.annotation.*;
-import org.springframework.boot.test.context.*;
-import org.springframework.boot.test.mock.mockito.*;
-import org.springframework.test.context.junit4.*;
-
-import static org.hamcrest.Matchers.containsString;
-
-import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
-@RunWith(SpringRunner.class)
+/**
+ * Test class for BinaryController, using Spring Boot's MockMvc for testing HTTP requests.
+ */
 @WebMvcTest(BinaryController.class)
+@ExtendWith(SpringExtension.class)
+@AutoConfigureMockMvc
 public class BinaryControllerTest {
 
     @Autowired
-    private MockMvc mvc;
+    private MockMvc mvc; // Simulates HTTP requests to the controller
+
+    @MockBean
+    private BinaryService binaryService; // Mocking the service layer for isolation
 
     /**
      * Test default GET request to ensure the calculator page loads correctly.
      */
     @Test
     public void getDefault() throws Exception {
-        this.mvc.perform(get("/"))
+        mvc.perform(get("/"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("calculator"))
                 .andExpect(model().attribute("operand1", ""))
@@ -46,7 +45,7 @@ public class BinaryControllerTest {
      */
     @Test
     public void getParameter() throws Exception {
-        this.mvc.perform(get("/").param("operand1", "1010"))
+        mvc.perform(get("/").param("operand1", "1010"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("calculator"))
                 .andExpect(model().attribute("operand1", "1010"))
@@ -58,8 +57,10 @@ public class BinaryControllerTest {
      * 1101 + 1010 = 10111
      */
     @Test
-    public void postParameter() throws Exception {
-        this.mvc.perform(post("/").param("operand1", "1101")
+    public void postAddition() throws Exception {
+        given(binaryService.calculate("1101", "+", "1010")).willReturn("10111");
+
+        mvc.perform(post("/").param("operand1", "1101")
                         .param("operator", "+")
                         .param("operand2", "1010"))
                 .andExpect(status().isOk())
@@ -73,8 +74,10 @@ public class BinaryControllerTest {
      * 11 * 110 = 10010
      */
     @Test
-    public void postMultiplyParameter() throws Exception {
-        this.mvc.perform(post("/").param("operand1", "11")
+    public void postMultiply() throws Exception {
+        given(binaryService.calculate("11", "*", "110")).willReturn("10010");
+
+        mvc.perform(post("/").param("operand1", "11")
                         .param("operator", "*")
                         .param("operand2", "110"))
                 .andExpect(status().isOk())
@@ -84,12 +87,31 @@ public class BinaryControllerTest {
     }
 
     /**
+     * Additional test for multiplication.
+     * 101 * 10 = 1010
+     */
+    @Test
+    public void postMultiplyAdditional() throws Exception {
+        given(binaryService.calculate("101", "*", "10")).willReturn("1010");
+
+        mvc.perform(post("/").param("operand1", "101")
+                        .param("operator", "*")
+                        .param("operand2", "10"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1010"))
+                .andExpect(model().attribute("operand1", "101"));
+    }
+
+    /**
      * Test bitwise AND operation.
      * 1110 & 1011 = 1010
      */
     @Test
-    public void postAndParameter() throws Exception {
-        this.mvc.perform(post("/").param("operand1", "1110")
+    public void postAnd() throws Exception {
+        given(binaryService.calculate("1110", "&", "1011")).willReturn("1010");
+
+        mvc.perform(post("/").param("operand1", "1110")
                         .param("operator", "&")
                         .param("operand2", "1011"))
                 .andExpect(status().isOk())
@@ -99,17 +121,53 @@ public class BinaryControllerTest {
     }
 
     /**
+     * Additional test for bitwise AND operation.
+     * 1101 & 1001 = 1001
+     */
+    @Test
+    public void postAndAdditional() throws Exception {
+        given(binaryService.calculate("1101", "&", "1001")).willReturn("1001");
+
+        mvc.perform(post("/").param("operand1", "1101")
+                        .param("operator", "&")
+                        .param("operand2", "1001"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1001"))
+                .andExpect(model().attribute("operand1", "1101"));
+    }
+
+    /**
      * Test bitwise OR operation.
      * 1001 | 1100 = 1101
      */
     @Test
-    public void postOrParameter() throws Exception {
-        this.mvc.perform(post("/").param("operand1", "1001")
+    public void postOr() throws Exception {
+        given(binaryService.calculate("1001", "|", "1100")).willReturn("1101");
+
+        mvc.perform(post("/").param("operand1", "1001")
                         .param("operator", "|")
                         .param("operand2", "1100"))
                 .andExpect(status().isOk())
                 .andExpect(view().name("result"))
                 .andExpect(model().attribute("result", "1101"))
                 .andExpect(model().attribute("operand1", "1001"));
+    }
+
+    /**
+     * Additional test for bitwise OR operation.
+     * 1010 | 0011 = 1011
+     */
+    @Test
+    public void postOrAdditional() throws Exception {
+        given(binaryService.calculate("1010", "|", "0011")).willReturn("1011");
+
+        mvc.perform(post("/").param("operand1", "1010")
+                        .param("operator", "|")
+                        .param("operand2", "0011"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("result"))
+                .andExpect(model().attribute("result", "1011"))
+                .andExpect(model().attribute("operand1", "1010"));
     }
 }
